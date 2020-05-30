18a0b9c63536fd5a0dc4e0f5ffd4fd0e700bd27f
byodian <byj6696@gmail.com>
2020-05-30 23:54:31 +0800

    Update bounces.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
-# bounce.py
-#
-# Exercise 1.5
+height = 100 # Meters
+times = 10
+num_bounces = 1
+
+while num_bounces <= times:
+    bounce_height = round(height * 3/5, ndigits=4)
+    print(num_bounces, bounce_height) 
+    height = bounce_height
+    num_bounces = num_bounces + 1
