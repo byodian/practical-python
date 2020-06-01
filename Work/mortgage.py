59fc80d354311c8f3157999a2273ca25751b112f
byodian <byj6696@gmail.com>
2020-06-01 21:28:18 +0800

    Exercise 1.9: Making an Extra Payment Calculator

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -13,17 +13,35 @@ total_paid = 0.0
 #print('Total paid', total_paid)
 
 # Exercise 1.8:Extra payments
+#extra_payment = 1000
+#extra_months = 12
+#
+#while principal > 0:
+#    if extra_months > 0:
+#        principal = principal * (1 + rate/12) - payment - extra_payment
+#        total_paid = total_paid + payment + extra_payment
+#    else:
+#        principal = principal * (1 + rate/12) - payment
+#        total_paid = total_paid + payment
+#    
+#    extra_months = extra_months - 1
+#
+#print('Total Paid', total_paid)
+
+# Exercise 1.9: Making an Extra Payment Calculator
+extra_payment_start_month = 60
+extra_payment_end_month = 108
 extra_payment = 1000
-extra_months = 12
+extra_months = 0
 
 while principal > 0:
-    if extra_months > 0:
-        principal = principal * (1 + rate/12) - payment - extra_payment
-        total_paid = total_paid + payment + extra_payment
-    else:
-        principal = principal * (1 + rate/12) - payment
-        total_paid = total_paid + payment
-    
-    extra_months = extra_months - 1
+   if extra_months >= 60 and extra_months <= 108:
+       principal = principal * (1 + rate/12) - payment - extra_payment
+       total_paid = total_paid + payment + extra_payment
+   else:
+       principal = principal * (1 + rate/12) - payment
+       total_paid = total_paid + payment
 
-print('Total Paid', total_paid)
+   extra_months = extra_months + 1
+
+print('Total paid', total_paid)
