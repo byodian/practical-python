591d504246d4e90bb15fcd52ee43d5a45a03eea0
byodian <byj6696@gmail.com>
2020-06-24 17:16:50 +0800

    Exercise 1.31: Error handling

--- Work/pcost.py
+++ Work/pcost.py
@@ -4,15 +4,24 @@
 import os
 from csv import reader
 
-total = 0
-with open('Data/portfolio.csv', 'r') as f:
-   csv_reader = reader(f)
-   header = next(csv_reader)
+def protfolio_cost(filename):
+    total = 0
+    with open(filename, 'r') as f:
+       csv_reader = reader(f)
+       header = next(csv_reader)
 
-   if header != None:
-       for row in csv_reader:
-            share = int(row[1])
-            price = float(row[2])
-            total += share * price
+       if header != None:
+           for row in csv_reader:
+               try:
+                    share = int(row[1])
+                    price = float(row[2])
+                    total += share * price
+               except ValueError:
+                    print('ValueError')
+    return total
 
-print('Total cost', total)
+cost1 = protfolio_cost('Data/portfolio.csv')
+print('Total cost', cost1)
+
+cost2 = protfolio_cost('Data/missing.csv')
+print('Total cost', cost2)
