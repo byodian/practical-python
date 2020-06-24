efaffa0098ec523bbe2665e99a2ca06a0e9722c3
byodian <byj6696@gmail.com>
2020-06-24 15:53:32 +0800

    Exercise 1.27: Reading a data file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,18 @@
 # pcost.py
 #
 # Exercise 1.27
+import os
+from csv import reader
+
+total = 0
+with open('Data/portfolio.csv', 'r') as f:
+   csv_reader = reader(f)
+   header = next(csv_reader)
+
+   if header != None:
+       for row in csv_reader:
+            share = int(row[1])
+            price = float(row[2])
+            total += share * price
+
+print('Total cost', total)
