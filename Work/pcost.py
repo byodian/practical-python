2211eda492d99590ee3f2267d3c159f3e6b6e1ac
byodian <byj6696@gmail.com>
2020-06-28 13:45:34 +0800

    Exercise 1.27: Reading a data file v2.0

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,27 +1,22 @@
 # pcost.py
 #
 # Exercise 1.27
-import os
-from csv import reader
-
 def protfolio_cost(filename):
     total = 0
-    with open(filename, 'r') as f:
-       csv_reader = reader(f)
-       header = next(csv_reader)
+    with open(filename, 'rt') as f:
+        header = next(f)
+        for line in f:
+            try:
+                line_list = line.split(',')
+                shares = int(line_list[1])
+                price = float(line_list[2])
+                total += shares * price
+            except ValueError:
+                print('ValueError')
+        return total
 
-       if header != None:
-           for row in csv_reader:
-               try:
-                    share = int(row[1])
-                    price = float(row[2])
-                    total += share * price
-               except ValueError:
-                    print('ValueError')
-    return total
+cost = protfolio_cost('Data/portfolio.csv')
+print('Total cost', cost)
 
-cost1 = protfolio_cost('Data/portfolio.csv')
+cost1 = protfolio_cost('Data/missing.csv')
 print('Total cost', cost1)
-
-cost2 = protfolio_cost('Data/missing.csv')
-print('Total cost', cost2)
