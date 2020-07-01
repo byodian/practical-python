886b940d3b435fdeb6ceac2c88f41de98a5f7f5d
byodian <byj6696@gmail.com>
2020-07-01 21:16:37 +0800

    Exercise 2.5: List of Dictionaries

--- Work/report.py
+++ Work/report.py
@@ -1,16 +1,36 @@
 # report.py
 #
 # Exercise 2.4: A list of tuples
-import csv
+#import csv
+#def read_portfolio(filename):
+#    with open(filename, 'rt') as file:
+#        portfolio = []
+#
+#        rows = csv.reader(file)
+#        headers = next(rows)
+#
+#        for row in rows:
+#           holding = (row[0], int(row[1]), float(row[2]))
+#           portfolio.append(holding)
+#
+#        return portfolio
+
+
+# Exercise 2.5: List of Dictionaries
+import  csv
+
 def read_portfolio(filename):
     with open(filename, 'rt') as file:
         portfolio = []
 
         rows = csv.reader(file)
         headers = next(rows)
-
         for row in rows:
-           holding = (row[0], int(row[1]), float(row[2]))
-           portfolio.append(holding)
+            holding = {
+                'name': row[0],
+                'shares': int(row[1]),
+                'price': float(row[2])
+            }
+            portfolio.append(holding)
 
         return portfolio
