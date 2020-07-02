fae2f89dde29f6f897db6dc7d5e0d7b622e88c92
byodian <byj6696@gmail.com>
2020-07-02 21:59:39 +0800

    Exercise 2.16: Using the zip() method

--- Work/pcost.py
+++ Work/pcost.py
@@ -5,19 +5,20 @@ import sys
 import csv
 
 def portfolio_cost(filename):
-    total = 0
+    total_cost = 0.0
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
 
         header = next(rows)
         for i, row in enumerate(rows, start = 1):
+            record = dict(zip(header, row))
             try:
-                shares = int(row[1])
-                price = float(row[2])
-                total += shares * price
+                shares = int(record['shares'])
+                price = float(record['price'])
+                total_cost += shares * price
             except ValueError:
                 print('Row {}: Couldn\'t convert: {}'.format(i, row))
-        return total
+        return total_cost
 
 # Exercise 1.33: Reading from the command line
 if len(sys.argv) ==2:
@@ -31,7 +32,7 @@ print('Total cost', cost)
 # Exercise 1.32: Using a library function
 from csv import reader
 
-total = 0
+total_cost = 0.0
 with open('Data/portfolio.csv', 'rt') as f:
     rows = reader(f)
     header = next(rows)
@@ -39,5 +40,5 @@ with open('Data/portfolio.csv', 'rt') as f:
     for row in rows:
         shares = int(row[1])
         price = float(row[2])
-        total += shares * price
-    print(total)
+        total_cost += shares * price
+    print(total_cost)
